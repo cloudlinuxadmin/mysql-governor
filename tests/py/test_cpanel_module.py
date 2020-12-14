c554bb3523d9637d30d4c80cff5289b1c9f82f4a
Sergey Kozhekin <skozhekin@cloudlinux.com>
2020-12-14 10:18:40 +0000

    MYSQLG-546: unittests
    Change-Id: Ic28a40c487cdcb5f52130d6ea1506c63b7cb92ec

--- tests/py/test_cpanel_module.py
+++ tests/py/test_cpanel_module.py
@@ -42,3 +42,57 @@ def test_update_user_map_exceptions(exception, expected_msg, capfd):
             assert cpanel.cPanelManager('').update_user_map_file()
         out, err = capfd.readouterr()
         assert expected_msg in out
+
+
+@pytest.mark.parametrize("mocked_content", [
+    'mariadb104',
+    'mariadb105',
+])
+@mock.patch("builtins.open", mock.mock_open(read_data=''))
+def test_for_unsupported_db_version(mocked_content):
+    """
+    This test is to check the response of the tested method to an unsupported
+    version of the database (expected "exit 1")
+    """
+    exit_mock = mock.MagicMock()
+    with mock.patch("modules.cpanel.sys.exit", exit_mock):
+        with mock.patch("modules.base.InstallManager._get_result_mysql_version",
+                        return_value=mocked_content):
+            cpanel.cPanelManager('').unsupported_db_version()
+    exit_mock.assert_called_once_with(1)
+
+
+@pytest.mark.parametrize("mocked_content", [
+    'mariadb104',
+    'mariadb105',
+])
+@mock.patch("builtins.open", mock.mock_open(read_data=''))
+def test_for_unsupported_db_version_with_force(mocked_content):
+    """
+    This test is to check the response of the tested method to an unsupported
+    version of the database (do nothing cause "force" key is passed)
+    """
+    exit_mock = mock.MagicMock()
+    with mock.patch("modules.cpanel.sys.exit", exit_mock):
+        with mock.patch("modules.base.InstallManager._get_result_mysql_version",
+                        return_value=mocked_content):
+            cpanel.cPanelManager('').unsupported_db_version(force=True)
+    exit_mock.assert_not_called()
+
+
+@pytest.mark.parametrize("mocked_content", [
+    'mariadb103',
+    'mysql57',
+])
+@mock.patch("builtins.open", mock.mock_open(read_data=''))
+def test_for_supported_db_version(mocked_content):
+    """
+    This test is to check the response of the tested method to a supported
+    version of the database (do nothing)
+    """
+    exit_mock = mock.MagicMock()
+    with mock.patch("modules.cpanel.sys.exit", exit_mock):
+        with mock.patch("modules.base.InstallManager._get_result_mysql_version",
+                        return_value=mocked_content):
+            cpanel.cPanelManager('').unsupported_db_version()
+    exit_mock.assert_not_called()
--- tests/py/test_panel_detection.py
+++ tests/py/test_panel_detection.py
@@ -16,3 +16,31 @@ from modules.da import DirectAdminManager
 def test_panel_detection(mocked_content, expected):
     res = InstallManager.factory(mocked_content)
     assert res.__class__ == expected
+
+managerlist = ['Unknown', 'DirectAdmin', 'Plesk', "ISPManager", "InterWorx"]
+@pytest.fixture(params=managerlist)
+def manager_instance(request):
+    """
+    Go through all the available panels (except cPanel)
+    """
+    with mock.patch("builtins.open", mock.mock_open(read_data='')):
+        m = InstallManager.factory(request.param)
+    return m
+
+@pytest.mark.parametrize("mocked_content", [
+    'mariadb104',
+    'mariadb105',
+    'mariadb103',
+    'mysql57',
+])
+def test_for_supported_db_version(manager_instance, mocked_content):
+    """
+    This test checks that the tested method does not affect for any
+    panels except cPanel
+    """
+    exit_mock = mock.MagicMock()
+    with mock.patch("modules.cpanel.sys.exit", exit_mock):
+        with mock.patch("modules.base.InstallManager._get_result_mysql_version",
+                        return_value=mocked_content):
+            manager_instance.unsupported_db_version()
+    exit_mock.assert_not_called()
