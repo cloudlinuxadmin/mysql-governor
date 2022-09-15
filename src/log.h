9f4002df45c19dee4bd2cf2f24429bcaf30dde04
Nikolay Petukhov <npetukhov@cloudlinux.com>
2022-09-15 15:29:55 +0200

    MYSQLG-734: 1. refactoring of logging functions
    Change-Id: I98b0e3520713911335c8359a848bc333a2ac6c57

--- src/log.h
+++ src/log.h
@@ -16,20 +16,18 @@
 
 int open_log (const char *log_file);
 int close_log (void);
-int write_log (const char *error_file, int error_line,
-	       const char *error_string, MODE_TYPE mode);
-char *prepare_output (char *buffer, size_t size, char *fmt, ...);
+int write_log (FILE *f, const char *error_file, int error_line, MODE_TYPE mode, Stats *limit, char *fmt, ...);
 
 FILE *get_log (void);
 FILE *get_restrict_log (void);
 FILE *get_slow_queries_log (void);
 
-#define WRITE_LOG(stats, type, buffer, size, message, mode, ...) if (type==0) \
-	write_log(__FILE__, __LINE__, prepare_output(buffer, size, message, ##__VA_ARGS__), mode); \
+#define WRITE_LOG(stats, type, buffer, size, fmt, mode, ...) if (type==0) \
+	write_log(get_log(), __FILE__, __LINE__, mode, stats, fmt, ##__VA_ARGS__); \
 else if (type==1) \
-	write_restrict_log (prepare_output(buffer, size, message, ##__VA_ARGS__), stats); \
-else if (type==2&&get_slow_queries_log())  \
-	write_slow_queries_log (prepare_output(buffer, size, message, ##__VA_ARGS__));
+	write_log(get_restrict_log(), __FILE__, __LINE__, mode, stats, fmt, ##__VA_ARGS__); \
+else if (type==2)  \
+	write_log(get_slow_queries_log(), __FILE__, __LINE__, mode, stats, fmt, ##__VA_ARGS__);
 
 //WRITE_LOG(NULL, 0, buffer, 2048, cfg->mode, "test %s", "Hello"); write to error_log
 //WRITE_LOG(stat1, 1, buffer, 2048, cfg->mode, "test %s", "Hello"); write to restrict log
@@ -38,14 +36,9 @@ else if (type==2&&get_slow_queries_log())  \
 
 int open_restrict_log (const char *log_file);
 int close_restrict_log (void);
-int write_restrict_log (const char *error_string, Stats * limits);
-
-int
-write_restrict_log_second_line (const char *error_string, int need_end_line);
 
 int open_slow_queries_log (const char *log_file);
 int close_slow_queries_log (void);
-int write_slow_queries_log (const char *error_string);
 
 void print_config (void *icfg);
 
